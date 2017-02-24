d7f72ac20780840fbd755470ba2be218f3956215
Ivan Kaplin <ikaplin@playtika.com>
2017-02-24 15:20:02 +0200

    Simple Zuul api gateway implementation

---
+++ src/main/java/com/ikaplin/zuul/filters/pre/LogIncomingRequestZuulFilter.java
@@ -0,0 +1,38 @@
+package com.ikaplin.zuul.filters.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class LogIncomingRequestZuulFilter extends ZuulFilter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogIncomingRequestZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        System.out.println("-------------------------------------------------------");
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        LOG.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        return true;
+    }
+
+}
